a68e74a50fbd8b7e459bb39ecd7c9841b104541c
Kirill Zverev <trics@icloud.com>
2024-04-18 09:56:32 +0300

    123

--- Sources/AdvancedCharts/LineChart/LineChartSettings.swift
+++ Sources/AdvancedCharts/LineChart/LineChartSettings.swift
@@ -9,28 +9,28 @@ import Foundation
 import SwiftUI
 
 public struct LineChartSettings {
-    var title: String = "Discofox Analytics"
-    var subTitle: String?
-    var lineWidth: CGFloat = 2
-    var showLineBlur: Bool = true
-    var showIndicators: Bool = false
-    var lineBackgroundBlurRadius: CGFloat = 5
-    var backgroundBlurLineWidth: CGFloat = 4
-    var showLineShadow: Bool = true
-    var gradientColor: LinearGradient = LinearGradient(colors: [Color(hexString: "0097F7"), Color(hexString: "D200D4")], startPoint: .bottom, endPoint: .top)
-    var curvedLines: Bool = true
-    var showLineBackground: Bool = true
-    var backgroundColor: Color = .black
-    var chartCornerRadius: CGFloat = 25
-    var lineShadowRadius: CGFloat = 6
-    var indicatorColor: Color = Color(hexString: "#FF57A6")
-    var indicatorStrokeColor: Color = .white
-    var indicatorStrokeWidth: CGFloat = 4
-    var indicatorWidth: CGFloat = 14
-    var showHorizontalGrid: Bool = true
-    var numberOfHorizontalGridLines: Int = 4
-    var isMaxValueOnTop: Bool = false
-    var linePoints: [Double]? = nil
+    public var title: String = "Discofox Analytics"
+    public var subTitle: String?
+    public var lineWidth: CGFloat = 2
+    public var showLineBlur: Bool = true
+    public var showIndicators: Bool = false
+    public var lineBackgroundBlurRadius: CGFloat = 5
+    public var backgroundBlurLineWidth: CGFloat = 4
+    public var showLineShadow: Bool = true
+    public var gradientColor: LinearGradient = LinearGradient(colors: [Color(hexString: "0097F7"), Color(hexString: "D200D4")], startPoint: .bottom, endPoint: .top)
+    public var curvedLines: Bool = true
+    public var showLineBackground: Bool = true
+    public var backgroundColor: Color = .black
+    public var chartCornerRadius: CGFloat = 25
+    public var lineShadowRadius: CGFloat = 6
+    public var indicatorColor: Color = Color(hexString: "#FF57A6")
+    public var indicatorStrokeColor: Color = .white
+    public var indicatorStrokeWidth: CGFloat = 4
+    public var indicatorWidth: CGFloat = 14
+    public var showHorizontalGrid: Bool = true
+    public var numberOfHorizontalGridLines: Int = 4
+    public var isMaxValueOnTop: Bool = false
+    public var linePoints: [Double]? = nil
     
     public init() {}
 }
